361aca8462408e1a840ab62dae7651c4088ab417
Emanuele F <emanueleffe@users.noreply.github.com>
2018-08-25 19:51:06 +0200

    added settings acquisition from settings.conf file

--- getUptime.py
+++ getUptime.py
@@ -5,18 +5,106 @@ import errno
 from subprocess import check_output
 import urllib2
 import re
-from datetime import datetime, timedelta
+from datetime import datetime,timedelta
 import sqlite3 as lite
 import plotly
 import platform
+import ConfigParser
+import os
+import platform
+import logging
 
-#global variables
-ip = '192.168.1.254'
-port = 23
-username = 'username'
-password = 'password'
-#used to check if I'm connected to my home network
-mySSID = 'ssid'
+
+def readConfig():
+    if os.path.isfile(settingsFilename):
+        config = ConfigParser.ConfigParser()
+        config.read(settingsFilename)
+        configDict = {}
+        for section in config.sections():
+            for(key,value) in config.items(section):
+                configDict[key] = value
+        # check if every read parameter is ok
+        validatedConfigDict = validateConf(configDict)
+    else:
+        createExampleFile()
+        validatedConfigDict = None
+    return validatedConfigDict
+
+def createExampleFile():
+    exampleFile = open(settingsFilename,"w")
+    filecontent = "[network]\nip = 192.168.1.254\nport = 23\nwirelessconnection = 1\n"
+    filecontent = filecontent + "myssid = ssid\n[login]\nusername = Administrator\n"
+    filecontent = filecontent + "password = password\n[plotly]\nonlinePlot = 0\n"
+    filecontent = filecontent + "[paths]\nLinuxPath = ~\nLinuxOutputpath = ~\n"
+    filecontent = filecontent + "WindowsPath = C:\n"
+    filecontent = filecontent + "WindowsOutputPath = C:\n"
+    filecontent = filecontent + "DBfilename = DSLuptime.db"
+    exampleFile.write(filecontent)
+
+def validateConf(configDict):
+    exc = ""
+
+    try:
+        #check if there's an empty parameter
+        for key in configDict:
+            if configDict[key] == '':
+                exc = "Empty " + key + " parameter"
+                raise Exception
+
+        #ip address evaluation
+        socket.inet_aton(configDict['ip'])
+
+        #port number evaluation (default telnet port number is 23)
+        if(not configDict['port'].isdigit() or
+           (int(configDict['port']) <= 0 or 
+            int(configDict['port']) > 65536)):
+            exc = "Invalid port number"
+            raise Exception
+        
+        #wirelessconnection evaluation
+        if(configDict['wirelessconnection'] != '0' and
+           configDict['wirelessconnection'] != '1'):
+           exc = "Invalid wirelessconnection option, please set it to 0 or 1"
+           raise Exception
+
+        #plot option evaluation
+        if(configDict['onlineplot'] != '0' and 
+           configDict['onlineplot'] != '1'):
+           exc = "Invalid onlineplot option, please set it to 0 or 1"
+           raise Exception
+
+        #path evaluation
+        if platform.system() == "Linux":
+            if (not os.path.isdir(configDict['linuxpath']) or
+                not os.path.isdir(configDict['linuxoutputpath'])):
+                exc = "Invalid path, the specified path or outputpath does not exist"
+                raise Exception
+        elif platform.system() == "Windows":
+            # raw_input and replace \ with /
+            windowsPath = raw_input(configDict['windowspath'])
+            windowsPath = windowsPath.replace("\\", "/")
+            
+            if (not os.path.isdir(windowsPath) or
+                not os.path.isdir(windowsPath)):
+                exc = "Invalid path, the specified path or outputpath does not exist"
+                raise Exception
+        else:
+            exc = "Not supported OS"
+            raise Exception
+        
+    #invalid ip address
+    except socket.error:
+        writeErrorLog("Invalid IP address")
+        configDict = None
+    #Generic exception
+    except Exception:
+        writeErrorLog(exc)
+        configDict = None
+    return configDict
+
+def writeErrorLog(errorString):    
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logging.error(" "+ now+ " - " + errorString)
 
 #function to check if there's an internet connection available
 def checkInternetConnection():
@@ -116,72 +204,107 @@ def insertDataToDBandCreateChart(collectedData):
             layout = plotly.graph_objs.Layout(showlegend=True)
             #create figure
             fig = plotly.graph_objs.Figure(data=data, layout=layout)
-            #offline plot
-            #plotly.offline.plot(fig, filename="DSL uptime chart.html", auto_open=False)
-            '''
-	        online plot: in order to use it you must signup on plot.ly website, and configure it
-            by using the following commands in python cli:
-            import plotly
-            plotly.tools.set_credentials_file(username='DemoAccount', api_key='lr1c37zw81')
-	        more info here: https://plot.ly/python/getting-started/#initialization-for-online-plotting
-            '''
-            plotly.plotly.plot(fig, filename="DSL uptime chart", auto_open=False)
-			
+
+            if(onlinePlot == True):
+                '''
+                online plot: in order to use it you must signup on plot.ly website, and configure it
+                by using the following commands in python cli:
+                import plotly
+                plotly.tools.set_credentials_file(username='DemoAccount', api_key='lr1c37zw81')
+                more info here: https://plot.ly/python/getting-started/#initialization-for-online-plotting
+                '''
+                plotly.plotly.plot(fig, filename="DSL uptime chart", auto_open=False)
+            else:
+                #offline plot
+                plotly.offline.plot(fig, filename="DSL uptime chart.html", auto_open=False)
+
     except lite.Error, e:
         print "Error: %s" %e.args[0]
     finally:
         if con:
             con.close()
 
+def getTelnetData():
+    try:
+        #declaration, initialization and beginning of telnet communication
+        tn = telnetlib.Telnet(config['ip'], config['port'])
+        #read until it finds "Username : "
+        tn.read_until("Username : ")
+        #write username + \r (carriage return)
+        tn.write(username + "\r")
+        tn.read_until("Password : ")
+        tn.write(password + "\r")
+        tn.write("system settime\r")
+        tn.write("xdsl info\r")
+        tn.write("exit\r")
+        #store the telnet output into output variable
+        output = tn.read_all()
+        tn.close()
+    except socket.error, v:
+        errorcode=v[0]
+        if errorcode==errno.ECONNREFUSED:
+            print("Connection refused")
+
+    return output
+
 #main function
 def main():
     collectedData = []
     #just comment out the second if, if there's no need to check ssid (e.g. wired connection)
     if(checkInternetConnection() & ((extractSSID() == mySSID) or extractSSID() == "Not Linux or Windows")):
-        try:
-            #declaration, initialization and beginning of telnet communication
-            tn = telnetlib.Telnet(ip,port)
-            #read until it finds "Username : "
-            tn.read_until("Username : ")
-            #write username + \r (carriage return)
-            tn.write(username + "\r")
-            tn.read_until("Password : ")
-            tn.write(password + "\r")
-            tn.write("system settime\r")
-            tn.write("xdsl info\r")
-            tn.write("exit\r")
-            #store the telnet output into output variable
-            output = tn.read_all()
-            tn.close()
+        output = getTelnetData()
 
-            date = ""
-            time = ""
-            #parse output to find the desired lines
-            for line in output.splitlines():
-                uptime = -1
-                if(re.match(r"date = [0-9]{2,2}/[0-9]{2,2}/[0-9]{4,4}", line)):
-                    date = formatDate(line[7:])
-                elif(re.match(r"time = [0-9]{2,2}:[0-9]{2,2}:[0-9]{2,2}", line)):
-                    time = line[7:-3]
-                elif(re.search(r":\s+ [0-9]+ giorno?i?,\s[0-9]{0,2}:[0-9]{2,2}:[0-9]{2,2}",line)):
-                    uptime = fromDaysToMinutes(line[33:])
-                
-                #if every desired data is collected we can proceed
-                if((date != "") and
-                    (time != "") and
-                    (uptime != -1)):
-                    eventDatetime = date + " " + time
-                    reconnectionDatetime = calcReconnection(eventDatetime,uptime)
-					#insertion of data to a list of tuples, then it's passed to insertDataToDBandCreateChart() function
-                    collectedData.append([eventDatetime, uptime, reconnectionDatetime])
-                    insertDataToDBandCreateChart(collectedData)
-
-        except socket.error, v:
-            errorcode=v[0]
-            if errorcode==errno.ECONNREFUSED:
-                print("Connection refused")
+        date = ""
+        time = ""
+        #parse output to find the desired lines
+        for line in output.splitlines():
+            uptime = -1
+            if(re.match(r"date = [0-9]{2,2}/[0-9]{2,2}/[0-9]{4,4}", line)):
+                date = formatDate(line[7:])
+            elif(re.match(r"time = [0-9]{2,2}:[0-9]{2,2}:[0-9]{2,2}", line)):
+                time = line[7:-3]
+            elif(re.search(r":\s+ [0-9]+ giorno?i?,\s[0-9]{0,2}:[0-9]{2,2}:[0-9]{2,2}",line)):
+                uptime = fromDaysToMinutes(line[33:])
+            
+            #if every desired data is collected we can proceed
+            if((date != "") and
+                (time != "") and
+                (uptime != -1)):
+                eventDatetime = date + " " + time
+                reconnectionDatetime = calcReconnection(eventDatetime,uptime)
+                #insertion of data to a list of tuples, then it's passed to insertDataToDBandCreateChart() function
+                collectedData.append([eventDatetime, uptime, reconnectionDatetime])
+                insertDataToDBandCreateChart(collectedData)
     else:
         print("Not connected to your home network or no network connection available")
-    
+
+
+#global variables
+settingsFilename = "settings.conf"
+logging.basicConfig(format='%(levelname)s:%(message)s',filename='errors.log',level=logging.DEBUG)
+
+config = readConfig()
+if config != None:
+    try:
+        ip = config['ip']
+        port = config['port']
+        username = config['username']
+        password = config['password']
+        isWirelessConnection = config['wirelessconnection']
+        if (isWirelessConnection == '1'):
+            isWirelessConnection = True
+            #used to check if I'm connected to my home network
+            mySSID = config['myssid']
+        else:
+            isWirelessConnection = False
+            mySSID = None
+        onlinePlot = config['onlineplot']
+        if(onlinePlot == '1'):
+            onlinePlot = True
+        else:
+            onlinePlot = False
+    except KeyError as e:
+        writeErrorLog('Missing config line')
+
 #use of functions
 main()
